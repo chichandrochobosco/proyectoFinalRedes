161f4c955a0cafc494475d5810b3189e5594cf17
Lisandro <Lobosco.lisandroet36@gmail.com>
2024-08-12 20:30:58 -0300

    pasado a netbeans

---
+++ ruleta/src/ruleta/ClienteChat.java
@@ -0,0 +1,36 @@
+
+package ruleta;
+import java.io.*;
+import java.net.*;
+
+public class ClienteChat {
+    private static final String HOST = "localhost"; // Cambia esto por la IP del servidor
+    private static final int PUERTO = 12345;
+
+    public static void main(String[] args) {
+        try (Socket socket = new Socket(HOST, PUERTO);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))) {
+
+            Thread lectorServidor = new Thread(() -> {
+                String fromServer;
+                try {
+                    while ((fromServer = in.readLine()) != null) {
+                        System.out.println(fromServer);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            lectorServidor.start();
+
+            String userInput;
+            while ((userInput = stdIn.readLine()) != null) {
+                out.println(userInput);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
